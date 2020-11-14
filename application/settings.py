b03408bd0cfcff306f3861928783bdb8f72d4651
Takuya Igei <28337009+itok01@users.noreply.github.com>
2020-11-14 14:01:41 +0900

    Install doc_viewer

--- application/settings.py
+++ application/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'doc_viewer.apps.DocViewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
