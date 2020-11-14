4bb46bcab5484f6fc176607419ca5e9d6c2d37ed
Takuya Igei <28337009+itok01@users.noreply.github.com>
2020-11-14 14:05:56 +0900

    Add doc_viewer/viewer

--- doc_viewer/views.py
+++ doc_viewer/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+
+def viewer(request: HttpRequest, document_id: int):
+    return render(request, 'doc_viewer/viewer.html', {})
