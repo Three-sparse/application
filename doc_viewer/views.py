c53a43c0f1098366cb3b4f4cf106a68392b8de70
Takuya Igei <28337009+itok01@users.noreply.github.com>
2020-11-15 10:54:28 +0900

    Add document viewer

--- doc_viewer/views.py
+++ doc_viewer/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import get_object_or_404, render
 
 
 def viewer(request: HttpRequest, document_id: int):
-    return render(request, 'doc_viewer/viewer.html', {})
+    context = {
+        'document_id': document_id,
+        'comment_user': 'Taro',
+        'comment_page': '1 / 2',
+        'comment_text': 'Hello',
+    }
+    return render(request, 'doc_viewer/viewer.html', context)
